ec3a28e2673fee94d5407f065997e0769efedb81
Norbert Waszkowiak <norbert.waszkowiak@gmail.com>
2021-06-18 21:15:56 +0200

    collector update

--- collect/collector.py
+++ collect/collector.py
@@ -1,25 +1,38 @@
+import asyncio
+
+from hateanalyzer.hate_analyzer import HateAnalyzer
 from settings.settings import get_parm
 import pandas as pd
 from datetime import datetime
 
 
-
 class Collector:
 
     def __init__(self):
-        self.interval = get_parm("collect_interval")
-        self.data_path = get_parm("csv_file_name")
-        self.hateanalyzer = HateAnalyzer()
+        self.__interval = get_parm("collect_interval")
+        self.__data_path = get_parm("csv_file_name")
+        self.__hateanalyzer = HateAnalyzer()
+        self.__run = False
 
-    def collect_data(self):
-        stats = self.hateanalyzer.get_stats()
+    def start(self):
+        self.__run = True
+        while self.__run:
+            await asyncio.sleep(self.__interval)
+            self.__collect_data()
+
+    def stop(self):
+        self.__run = False
+
+    async def __collect_data(self):
+        stats = self.__hateanalyzer.get_stats()
         try:
-            df_old = pd.read_csv(self.data_path)
+            df_old = pd.read_csv(self.__data_path)
         except FileNotFoundError:
             df_old = pd.DataFrame()
             df_old.index.name = "Index"
-        newValuesDf = pd.DataFrame(data=stats)
-        newValuesDf['Time'] = pd.Series([datetime.now() for x in range(len(newValuesDf.index))])
-        df2 = pd.concat([df_old, newValuesDf])
+        new_values_df = pd.DataFrame(data=stats)
+        now = datetime.now()
+        new_values_df['Time'] = pd.Series([now for x in range(len(new_values_df.index))])
+        df2 = pd.concat([df_old, new_values_df])
 
-        df2.to_csv(self.data_path,index=False)
+        df2.to_csv(self.__data_path, index=False)
--- collect/main.py
+++ collect/main.py
@@ -1,9 +1,13 @@
+import asyncio
+
 import pandas as pd
 
 from collect.collector import Collector, HateAnalyzer
 
 if __name__ == "__main__":
-    stats = Collector().collect_data()
+    collector = Collector()
+    collector.start()
+    await asyncio.sleep(1000)
+    collector.stop()
 
-    print(stats)
 
