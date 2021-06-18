26d4bb02a3b539e9341d5ee25aff693de9402d90
Norbert Waszkowiak <norbert.waszkowiak@gmail.com>
2021-06-19 00:22:35 +0200

    analysis update

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -11,16 +11,19 @@ class Analysis:
     def __init__(self):
         self.__last_datatime = None
         self.__interval = get_parm('analysis_interval')
-        self.path_to_data = get_parm('csv_file_name')
+        self.__path_to_data = get_parm('csv_file_name')
+        self.__limit_hate_ratio = get_parm('limit_hate_ratio')
+        self.__limit_hate_sum = get_parm('limit_hate_value')
         self.__run = False
 
     def start(self):
         def loop():
             while self.__run:
                 sleep(self.__interval)
-                t = threading.Thread(target=self.get_stats_for_last_period(), name="get_stats_for_last_period")
+                t = threading.Thread(target=self.__get_stats_for_last_period(), name="__get_stats_for_last_period")
                 t.daemon = True
                 t.start()
+
         self.__run = True
         t = threading.Thread(target=loop, name="loop")
         t.daemon = True
@@ -29,16 +32,23 @@ class Analysis:
     def stop(self):
         self.__run = False
 
-    def get_stats_for_last_period(self):
-        df = pd.read_csv(self.path_to_data)
+    def __get_stats_for_last_period(self):
+        df = pd.read_csv(self.__path_to_data)
         df['Time'] = pd.to_datetime(df['Time'])
         newest = df['Time'].max()
-        last = (newest - timedelta(seconds=self.__interval) + timedelta(milliseconds=10)) if self.__last_datatime is None else self.__last_datatime
-
+        last = (newest - timedelta(seconds=self.__interval) + timedelta(
+            milliseconds=10)) if self.__last_datatime is None else self.__last_datatime
         mask = (df['Time'] > last) & (df['Time'] <= newest)
         sub_df = df.loc[mask]
-        print("DFAFDS")
-        print(sub_df)
-        print("DFAFDS")
+        self.__analise_sub_data(sub_df)
 
+    def __analise_sub_data(self, df: pd.DataFrame):
+        hate_sum = 0
 
+        for row in df.itertuples():
+            hate_ratio = row.offensive_language + row.hate_speech
+            if hate_ratio >= self.__limit_hate_ratio:
+                hate_sum = hate_sum + 1
+        if hate_sum > self.__limit_hate_sum:
+            return True
+        return False
