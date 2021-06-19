b0e083b1aed49f25220901478c1a2bb1928cb29d
Joberooo <patryk.barczak@student.wat.edu.pl>
2021-06-19 12:49:26 +0200

    Add sidebar, style to sidebar and template for other pages

--- gui/main.py
+++ gui/main.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/settings")
+def settings():
+    return render_template("settings.html", conten=None)
+
+
+@app.route("/hate")
+def hate():
+    return render_template("hate.html", content=None)
+
+
 @app.route("/clientPanel")
 def client():
     return render_template("clientPanel.html", content=None)
