38ae6266358d9841486587f52169952f3b735b74
piotr <piotr.ciecwierz@gmail.com>
2021-06-19 02:21:43 +0200

    Added buffer list for hate_analyzer

--- hateanalyzer/hate_analyzer.py
+++ hateanalyzer/hate_analyzer.py
@@ -1,7 +1,9 @@
 from textRecognitionFromImage.TextRecognition import TextRecognition
+from settings.settings import get_parm
 
 from hatesonar import Sonar
 import re
+
 
 class HateAnalyzer:
     def __init__(self):
@@ -13,12 +15,29 @@ class HateAnalyzer:
 
         self.sonar = Sonar()
 
+        self.dictionary = []
+
+    def _check_dict(self, string):
+        MAX_LEN = get_parm('list_len')
+        ret_value = False
+        if string in self.dictionary:
+            self.dictionary.remove(string)
+            self.dictionary = [string] + self.dictionary
+            ret_value = True
+        else:
+            self.dictionary = [string] + self.dictionary
+
+        if len(self.dictionary) > MAX_LEN:
+            self.dictionary.pop(MAX_LEN)
+
+        return ret_value
+
     def get_stats(self):
         output_dicts = []
         text_rows = self.text_rec.recognize()
         for row in text_rows:
             is_not_whitespace = re.search("\S\S\S", row)
-            if is_not_whitespace:
+            if is_not_whitespace and not self._check_dict(row):
                 row_data = self.sonar.ping(row)
                 output_data = {}
                 for analyze_class in row_data['classes']:
